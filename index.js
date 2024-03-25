c037d4a8332dd862df955b02cc2edd4f754692f4
Muhammad Hammad <160263701+Hammadu508@users.noreply.github.com>
2024-03-25 18:31:26 +0500

    Created number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answere = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Pleas guess a number between 1-6:",
+    },
+]);
+if (answere.userGuessedNumber === randomNumber) {
+    console.log("Congtratulation! you guessed right number.");
+}
+else {
+    console.log("You guessed wrong number");
+}
